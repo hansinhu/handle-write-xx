0ab1ba4dcd9d6504980cecff516ec9d6a68f455a
TianYuan <tianyuan.hu@tuya.com>
2021-03-26 10:09:34 +0800

    1223

--- react/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { h, Fragment, h as createElement } from './h'
-export { render } from './reconciler'
-export { useState, useReducer, useEffect, useMemo, useCallback, useRef, useLayout, useLayout as useLayoutEffect } from './hooks'
-export { shouldYield } from './scheduler'
-export {lazy,Suspense} from './suspense'
-export * from './type'
-
-export const options: Record<string, Function> = {}
